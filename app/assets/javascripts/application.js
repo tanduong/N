94e772d0e5d65e4464703e41a786424f332dc337
TanD <tan.duong@eastagile.com>
2014-09-24 01:03:57 +0700

    Use angular-ui/ui-map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,6 @@
 //= require bootstrap-sass-official
 //= require angular/angular
 //= require angular-route/angular-route
+//= require angular-ui-utils/ui-utils
+//= require angular-ui-map/ui-map
 //= require_tree .
